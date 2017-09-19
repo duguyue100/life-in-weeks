18608a090e4ac2b05ea1fb7cb135f691526b6046
Yuhuang Hu <duguyue100@gmail.com>
2017-09-19 02:01:02 +0200

    update

--- dates.js
+++ dates.js
@@ -1,10 +1,10 @@
 var dates =
 { "W1": "#A5D2FF: Years that have no memory"
-, "W330": "#81C0C0: Elementary School"
-, "W599": "#9CD793: Middle School"
-, "W755": "#FFFF93: High School"
-, "W964": "#FFC68C: College"
+, "W331": "#81C0C0: Elementary School"
+, "W635": "#9CD793: Junior High School"
+, "W791": "#FFFF93: High School"
+, "W974": "#FFC68C: A Year Break"
 //, "2002-02-02": "First Child"
-, "W1173": "#FF9B9B: Carreer"
+, "W1049": "#FF9B9B: Carreer"
 , "W3288": "#C7B4D9: Retirement"
 };
